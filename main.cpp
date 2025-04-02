5e9e97ade0c8b5126d824cc44c2be720322a8392
Ant0CPU <antonio.bruno@marconibari.edu.it>
2025-04-02 16:21:07 +0000

    Consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,66 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <ctime>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
-}
+    int n, i, j, t;
+    string s;
+    bool scambi;
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
+    scambi=true;
+    i=0;
+    
+    do {
+        cout<<"Inserire il numero di automobili"<<endl;
+        cin>>n;
+    } while(n<=0);
+
+    vector<string> nomi(n);
+    vector<float> pos(n);
+
+    for (i=0; i<n; i++) {
+        
+        cout<<"Inserisci nomi"<<endl;
+        cin>>nomi[i];
+
+        pos[i]= 1+rand() % 361;
+    }
+
+    i=0;
+    while (i<n-1 and scambi==true) {
+        scambi=false;
+        j=0;
+
+        while (j<=(n-2)-i) {
+            
+            if (pos[j]<pos[j+1]) {
+                
+                t=pos[j];
+                pos[j]=pos[j+1];
+                pos[j+1]=t;
+
+                s=nomi[j];
+                nomi[j]=nomi[j+1];
+                nomi[j]=s;
+
+                scambi=true;
+            }
+
+            j=j+1;
+        }
+        
+        i=i+1;
+    }
+
+    cout<<"Nomi:";
+    for (i=0; i<n; i++) {
+        cout<<nomi[i]<<"; ";
+    }
+
+    cout<<"Posizione: ";
+    for (i=0; i<n; i++) {
+        cout<<pos[i]<<"; ";
+    }
+}
